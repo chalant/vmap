4fbff2ed23b3efc59e30132ca4aaf456632de3c6
chalant <chalantvsy@gmail.com>
2021-11-18 13:05:23 +0100

    workspace fix

--- src/gscrap/projects/workspace.py
+++ src/gscrap/projects/workspace.py
@@ -1,9 +1,15 @@
 from os import path
+from os import mkdir
 
 class WorkSpace(object):
     def __init__(self, working_dir, project_name):
         self._working_dir = working_dir
-        self._project_dir = path.join(working_dir, project_name)
+        self._project_dir = project_dir = path.join(working_dir, project_name)
+
+        try:
+            mkdir(project_dir)
+        except FileExistsError:
+            pass
 
     @property
     def working_dir(self):
