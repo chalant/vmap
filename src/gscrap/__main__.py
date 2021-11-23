31e2ff0966ca18562ca4892f84bdc15464577332
chalant <chalantvsy@gmail.com>
2021-11-23 21:56:59 +0100

    requirements

--- src/gscrap/__main__.py
+++ src/gscrap/__main__.py
@@ -7,9 +7,10 @@ def cli():
     pass
 
 @cli.command()
+@click.argument("project_name")
 @click.argument("directory")
-def run(directory):
-    gmap.launch(directory)
+def run(project_name, directory):
+    gmap.launch(project_name, directory)
 
 if __name__ == '__main__':
     cli()
